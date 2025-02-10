5a6769d8a75d1d27319c3656433de2e15d4b52b8
Sourabh Verma <63900063+Sourabhverma844@users.noreply.github.com>
2025-02-10 14:13:36 +0000

    container config done

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const buttons = document.querySelectorAll('.button');
-const body = document.querySelector('body')
+const buttons = document.querySelectorAll('.button');  //here we store .button class in const buttons
+const body = document.querySelector('body')            //here we are storing body in const body
 
+//here we applied forEach on buttons           .forEach(function (item){callbackfunction})
 buttons.forEach(function (button) {
     console.log(button);
-    button.addEventListener('click',function(e){
-        console.log(e);
-        console.log(e.target);
-        if (e.target.id === 'grey'){
+    button.addEventListener('click',function(e){            //here we add a eventlistner clcik on button function(yaha hum event ko ek nam de denge){callback}
+        console.log(e);                                     //e.target tells ki kis target se hamare pas event aya he
+        console.log(e.target);                             
+        if (e.target.id === 'grey'){                        //e.target.id jis target se hamare pas event aya uski id kya he
             body.style.backgroundColor = e.target.id;
         }
         if (e.target.id === 'white'){
